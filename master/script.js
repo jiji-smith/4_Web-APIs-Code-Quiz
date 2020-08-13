c2dc8d637a501e839b6254e580b116357d59004e
Ji Smith <jihyunjung0219@gmail.com>
2020-08-13 15:59:50 -0500

    made quiz functions work, added some CSS styles

--- master/script.js
+++ master/script.js
@@ -1,6 +1,6 @@
 
 //variables from HTML - all the id
-const startBtn = document.querySelector("#startBtn");
+let startBtn = document.querySelector("#startBtn");
 let showTimer = document.querySelector(".showtimer");
 let quiz = document.querySelector("#quiz");
 let question = document.querySelector("#question");
@@ -11,26 +11,36 @@ let choiceB = document.querySelector("#B");
 let choiceC = document.querySelector("#C");
 let choiceD = document.querySelector("#D");
 let scoreContainer = document.querySelector("#scoreContainer");
-let nameInput = document.querySelector("#name")
+let nameInput = document.querySelector("#name");
+let showTheAnswer = document.querySelector("#trueOrFalse");
 
 
 // timer
-let secondsLeft = 4;
-let score = 0;
+let secondsLeft = 900;
 
 
-startBtn.addEventListener("click", function(){
+// Start Button
+startBtn.addEventListener("click", start)
+
+function start(event) {
+  event.preventDefault();
+  event.stopPropagation();
   setTime();
-})
+  quiz.setAttribute("style", "display : block");
+  renderQuestion();
+}
 
 function setTime() {
-  var timerInterval = setInterval(function() {
+  let timerInterval = setInterval(function() {
     if (secondsLeft > 0) {
       secondsLeft--;
-      showTimer.textContent = "you have " + secondsLeft + "seconds left";
+      showTimer.textContent = "  You have " + secondsLeft + " seconds left!";
     } else {
       clearInterval(timerInterval);
+      // how to hide questions when it hits 0
+      quiz.setAttribute("style", "display: none")
       alert("Time Out!");
+      ;
     }
   }, 1000);
 }
@@ -39,47 +49,47 @@ function setTime() {
 
 let questions = [
   {
-    question : "what is JS stands for",
+    question : "Q1. what does JS stands for",
     // imgSrc : "img/html.png",
     choiceA : "Ji Smith",
-    choiceB : "Jola Ssagaji",
+    choiceB : "Jurassic Story",
     choiceC : "Java Script",
-    choiceD : "I don know",
+    choiceD : "Just Saying",
     correct : "C"
   },
   {
-    question : "question2",
+    question : "Q2. What is the command to display a prompt?",
     // imgSrc : "img/html.png",
-    choiceA : "Ji Smith",
-    choiceB : "Jola Ssagaji",
-    choiceC : "Java Script",
-    choiceD : "I don know",
-    correct : "C"
+    choiceA : "prompt('text here')",
+    choiceB : "prompt['text here']",
+    choiceC : "command.prompt()",
+    choiceD : "go,prompt!",
+    correct : "A"
   },
   {
-    question : "question3",
+    question : "Q3. What values can confirm() return?",
     // imgSrc : "img/html.png",
-    choiceA : "Ji Smith",
-    choiceB : "Jola Ssagaji",
-    choiceC : "Java Script",
-    choiceD : "I don know",
-    correct : "C"
+    choiceA : "NaN",
+    choiceB : "Null",
+    choiceC : "string",
+    choiceD : "Ture or False",
+    correct : "D"
   },
   {
-    question : "question 4",
+    question : "Q4.  Where is the correct place to insert a JavaScript? ",
     // imgSrc : "img/html.png",
-    choiceA : "Ji Smith",
-    choiceB : "Jola Ssagaji",
-    choiceC : "Java Script",
-    choiceD : "I don know",
-    correct : "C"
+    choiceA : "Both the &lt;head&gt; section and the &lt;body&gt; section are correct",
+    choiceB : "The &lt;body&gt; section",
+    choiceC : "The &lt;head&gt; section",
+    choiceD : "Neither of them",
+    correct : "A"
   }
 ];
 
 
-
+//Render Questions from the questions list
 let lastQuestionIndex = questions.length - 1;
-let runningQuestionIndex = 0;
+let runningQuestionIndex= 0;
 
 function renderQuestion(){
   let eachQuestion = questions[runningQuestionIndex];
@@ -89,23 +99,31 @@ function renderQuestion(){
   choiceB.innerHTML = eachQuestion.choiceB;
   choiceC.innerHTML = eachQuestion.choiceC;
   choiceD.innerHTML = eachQuestion.choiceD;
+}
+runningQuestionIndex;
+
+
+/*
+function checkAnswer(event){
+  console.log(event);
+  let q = questions[runningQuestionIndex];
+  let correctChoice = q.correct;
+}
+ let playerChoice =
 
 }
 
-renderQuestion();
-runningQuestionIndex ++;
-renderQuestion();
+*/
 
 
-
-
-
+let score = 0;
 function checkAnswer(answer){
   if(questions[runningQuestionIndex].correct === answer){
     score++;
-    score.textContent = scoreContainer;
+    scoreContainer.textContent = score;
+    alert("correct");
   } else {
-    alert("wrong~");
+    alert("wrong");
   }
   if(runningQuestionIndex < lastQuestionIndex){
     count = 0;
@@ -117,6 +135,16 @@ function checkAnswer(answer){
 };
 
 /*
+scoreContainer;
+
+
+if (secondsLeft === "0") {
+  nameInput.setAttribute("style", "display : block");
+  nameInput.innerHTML;
+}
+
+/*
+
 function scoreRender(){
   scoreContainer.style.display="block";
   let scorePercent = Math.round(100 * score/questions.length);
